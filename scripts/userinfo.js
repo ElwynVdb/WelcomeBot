7d889f3dae06bc94c5349fcf37241b9dc9352c61
Josia50 <elwyn.vdb@gmail.com>
2018-07-18 20:24:16 +0200

    Userinfo and water change

--- scripts/userinfo.js
+++ scripts/userinfo.js
@@ -11,19 +11,14 @@ client.on('ready', () => {
     console.log('Userinfo is ready!');
 })
 
-client.on("message", (message) => {
+client.on("message", (message, msg) => {
+
+    var messageText = message.content.toUpperCase(); 
 
 
-    var API = message.content.toUpperCase(); 
-     
-    let command = message.content.toUpperCase().split(" ")[0];
-   
-    let args = message.content.split(" ").slice(1);
-    
-
-    if (API == "+USERINFO") {
-        if(!message.member.roles.some(r=>["DMU Staff", "SWD-Developer"].includes(r.name)) ) return;
-        var user = message.mentions.users.first();
+    if (messageText == "+USERINFO") {
+   const member = msg.mentions.members.first() || msg.guild.members.get(args[0]) || msg.member;
+  if (!member) return msg.reply("Please provide a vaild Mention or USER ID");
         var embed = new Discord.RichEmbed();
         if (!user) {
           embed.addField("Username", `${message.author.username}#${message.author.discriminator}`, true)
@@ -42,51 +37,14 @@ client.on("message", (message) => {
               message.channel.sendEmbed(
                   embed, {
                       disableEveryone: true
-                  }
-              );
-          } else {
-          embed.addField("Username", `${user.username}#${user.discriminator}`, true)
-              .addField("ID", `${user.id}`, true)
-              .setColor(randomcolor())
-              .setFooter(' ', ' ')
-              .setThumbnail(`${user.avatarURL}`)
-              .setTimestamp()
-              .setURL(`${user.avatarURL}`)
-              .addField('Currently', `${user.presence.status}`, true)
-              .addField('Game', `${user.presence.game === null ? "No Game" : user.presence.game.name}`, true)
-              .addField('Joined Discord', `${moment(user.createdAt).format('MM.DD.YY')}`, true)
-              .addField('Is Bot', `${user.bot}`, true)
-        message.channel.sendEmbed(
-            embed, {
-                disableEveryone: true
-            }
-        );
-      }
-    } else
-  
-      if (API == "+SERVERINFO ") {
-        if(!message.member.roles.some(r=>["DMU Staff", "SWD-Developer"].includes(r.name)) )
-        var embed = new Discord.RichEmbed();
-        embed.addField("Server Name", `${message.guild.name}`, true)
-            .addField("Server ID", `${message.guild.id}`, true)
-            .setColor(randomcolor())
-            .setFooter(' ', ' ')
-            if (message.guild.iconURL) embed.setThumbnail(`${message.guild.iconURL}`);
-            if (message.author.avatarURL) embed.setURL(`${message.author.avatarURL}`);
-        embed.setTimestamp()
-            .addField('Guild Owner', `${message.guild.owner.user.username}`, true)
-            .addField('Owner ID', `${message.guild.owner.id}`, true)
-            .addField('Guild Created', `${moment(message.guild.createdAt).format('MM.DD.YY')}`, true)
-            .addField('Roles', `${message.guild.roles.filter(r => r.name).size}`, true)
-            .addField('Verification Level', `${message.guild.verificationLevel}`, true)
-            .addField('Region', `${message.guild.region}`, true)
-        message.channel.sendEmbed(
-            embed, {
-                disableEveryone: true
-            }
-        
-        );
-    } 
-})  
+                  })
+    }  
+}
+})
+
+
+
+
+
 
 client.login(config.token);
