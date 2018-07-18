321e73ea7d1cfb3b63fbe4b30a9b53fab62cef05
Glitch (lukebeforeyoubot) <none>
2018-07-18 14:29:22 +0000

    🎟🍑 Updated with Glitch

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -7,30 +7,44 @@ client.on('ready', () => {
     console.log('Welcome/Leave Message enabled!')
 })
 
-const getDefaultChannel = async (guild) => {
-    // get "original" default channel
-    if(guild.channels.has(guild.id))
-      return guild.channels.get(guild.id)
-  
-    // Check for a "general" channel, which is often default chat
-    if(guild.channels.exists("name", "general"))
-      return guild.channels.find("name", "general");
-    // Now we get into the heavy stuff: first channel in order where the bot can speak
-    // hold on to your hats!
-    return guild.channels
-     .filter(c => c.type === "text" &&
-       c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
-     .sort((a, b) => a.position - b.position ||
-       Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
-     .first();
-  }
+
+//Do you even use getdefaultChannel
+// i use a workaround since it doesn't work anymore 
+//then we can just delete it?
+// okay :)
 
   client.on("guildMemberAdd", (member) =>  {
     member.guild.channels.find("name", "general").send(`${member.user}, Welcome to hell it's great here pull up a chair and have a pepsi`);
   });
 
   client.on("guildMemberRemove", (member, message) =>  {
-    member.guild.channels.find("name", "general").send(`${member.user} has left!`);
+    member.guild.channels.find("name", "general").send("Takes ${member.user}`s pepsi back!");
   });
 
+client.on('message', message => {
+   if (message.author.id == "318821976372150272" || message.author.id == "338717002879336461") {
+    switch(message.content.toLowerCase()) {
+        case '-reboot':
+            resetBot(message.channel);
+        message.author.sendMessage("is this a thing?");
+            break;
+    }
+   }
+  });
+
+
+
+  // Turn bot off (destroy), then turn it back on
+  function resetBot(channel) {
+    // send channel a message that you're resetting bot [optional]
+    channel.send('Resetting Welcome messages')
+    .then(msg => client.destroy())
+    .then(() => client.login(config.token));
+  }
+
+
+
+
+
   client.login(config.token);
+               
