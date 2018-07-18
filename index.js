8c4527cde9b113dd4834593edd4a2559a09913da
Josia50 <elwyn.vdb@gmail.com>
2018-07-18 09:18:31 +0200

    Fix

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const http = require('http');
+const config = require("./config.json");
 const express = require('express');
 const app = express();
-const http = require('http')
-let config = require("./config.json")
-let Eris = require('eris');
-let Discord = require('discord.js')
-let c = new Discord.Client();
 
 
-c.on('guildMemberAdd', (guild, member) => {
-    c.createMessage(guild.defaultChannel.id, `${member.user.mention}, Welcome to hell its great here pull up a chair and have a pepsi.`);
-});
-
-c.on('error', console.log)
-
-c.login(config.token);
