033a467490e5431d823e68fe76054056c21b87a3
Josia50 <elwyn.vdb@gmail.com>
2018-07-19 20:08:11 +0200

    Sarah command

--- scripts/answers.js
+++ scripts/answers.js
@@ -51,8 +51,9 @@ var messageText = message.content.toUpperCase();
 }
   
 if (messageText == "+SARAH") {
-if (!message.author.id == "318821976372150272" || !message.author.id == "338717002879336461" || !message.author.id == "167922295556407296") return;
+if (message.author.id == "318821976372150272" || message.author.id == "338717002879336461" || message.author.id == "167922295556407296") {
 message.reply("Sarah is Sub's angel!")
+}
 }
 
 })
