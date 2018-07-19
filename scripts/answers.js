be645722298482317e9dda678b3e50bf620e6c88
Josia50 <38124619+Josia50@users.noreply.github.com>
2018-07-19 06:32:42 +0200

    Merged Mew's Code into master
    Do we need this?

--- scripts/answers.js
+++ scripts/answers.js
@@ -34,6 +34,46 @@ var messageText = message.content.toUpperCase();
    if (messageText == "ished") {
        message.reply("who's that")
    }
+    
+ //Added "WHO MADE YOU?"
+//Added "I DON'T LIKE WATER"
+//Added "I DON'T LIKE MILK"
+//Added "WHAT IS A MEW?"
+//Added "WHAT IS MEW BOT?"
+//Added sentence including"YOGSCAST"
+//Added sentence start with"FORTNITE"
+//Note - Anything starting with + is a command, most of these are tests, and will be removed if they do not work
+
+if (messageText == "WHO MADE YOU?") {
+    message.reply("Some lovely people by the name of Josia, Luke, Sub, Ished and Mew!")
+}
+if (messageText === "+NAMEMC") {
+message.channel.sendMessage('https://namemc.com/'+args.join(" "))
+};
+if (messageText == "I DON'T LIKE WATER") {
+message.reply("Milk?")
+}
+if (messageText == "I DON'T LIKE MILK") {
+message.reply("Well then you don't survive you fussy shite!")
+}
+if (messageText == "WHAT IS A MEW?") {
+message.reply("Mew is a pokémon... oh, you mean that one, he's uh, coder/texture hacker thing, for minecraft, discord, and nintendo games, just ignore him...")
+}
+if (messageText === "+3DSKIN") {
+message.channel.sendFile('https://visage.surgeplay.com/full/512/'+args.join(" "),'askinforyouminecraftnerds.png')
+};
+if (messageText === "+SKIN") {
+message.channel.sendFile('https://visage.surgeplay.com/skin/'+args.join(" "),'iswearimnotstealingfromsubimremakingwithsamewebsitesshutup.png')
+};
+if (messageText == "WHAT IS MEW BOT?") {
+message.reply("We do not discuss Mew Bot")
+}
+if (message.Include == "YOGSCAST") {
+message.reply("Welcome to hell, again")
+}
+if (message.content.startsWith == "FORTNITE") {
+message.channel.sendFile('https://thumbs.gfycat.com/FineDampGecko-size_restricted.gif', 'intensification.gif')
+}
 
     }
 );
