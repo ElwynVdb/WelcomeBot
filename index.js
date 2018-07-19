2dd2413d725bb17cbb12271314593cb13dca7ad1
Josia50 <elwyn.vdb@gmail.com>
2018-07-19 20:01:49 +0200

    ree

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
- if (message.content.startsWith('/DMU'))
+ if (message.content.includes('/DMU'))
      message.delete();
 })
 
