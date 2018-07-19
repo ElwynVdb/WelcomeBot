06755852488a5ef66433e33840b8deb4f2b17b0e
Josia50 <elwyn.vdb@gmail.com>
2018-07-19 08:08:38 +0200

    Mew update

--- scripts/answers.js
+++ scripts/answers.js
@@ -42,46 +42,6 @@ if (messageText == "+SARAH") {
 if (!message.author.id == "318821976372150272" || !message.author.id == "338717002879336461" || message.author.id == "167922295556407296") return;
 message.reply("Sarah is Sub's angel!")
 }
-   //Mew 
- //Added "WHO MADE YOU?"
-//Added "I DON'T LIKE WATER"
-//Added "I DON'T LIKE MILK"
-//Added "WHAT IS A MEW?"
-//Added "WHAT IS MEW BOT?"
-//Added sentence including"YOGSCAST"
-//Added sentence start with"FORTNITE"
-//Note - Anything starting with + is a command, most of these are tests, and will be removed if they do not work
-
-if (messageText == "+creators") {
-    message.reply("Some lovely people by the name of Josia, Luke, Sub, Ished and Mew!")
-}
-if (messageText === "+NAMEMC") {
-message.channel.sendMessage('https://namemc.com/'+args.join(" "))
-};
-if (messageText == "I DON'T LIKE WATER") {
-message.reply("Milk?")
-}
-if (messageText == "I DON'T LIKE MILK") {
-message.reply("Well then you don't survive you fussy shite!")
-}
-if (messageText == "WHAT IS A MEW?") {
-message.reply("Mew is a pokémon... oh, you mean that one, he's uh, coder/texture hacker thing, for minecraft, discord, and nintendo games, just ignore him...")
-}
-if (messageText === "+3DSKIN") {
-message.channel.sendFile('https://visage.surgeplay.com/full/512/'+args.join(" "),'askinforyouminecraftnerds.png')
-};
-if (messageText === "+SKIN") {
-message.channel.sendFile('https://visage.surgeplay.com/skin/'+args.join(" "),'iswearimnotstealingfromsubimremakingwithsamewebsitesshutup.png')
-};
-if (messageText == "WHAT IS MEW BOT?") {
-message.reply("We do not discuss Mew Bot")
-}
-if (messageText == "YOGSCAST") {
-message.reply("Welcome to hell, again")
-}
-if (messageText == "FORTNITE") {
-message.channel.sendFile('https://thumbs.gfycat.com/FineDampGecko-size_restricted.gif', 'intensification.gif')
-}
 })
 
 client.on('message', message => {
