93e956eaea5b509755dc4e4e86a219fbf4cf3747
Josia50 <elwyn.vdb@gmail.com>
2018-07-20 08:13:01 +0200

    reee

--- scripts/help.js
+++ scripts/help.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const express = require('express');
 const config = require("./config.json");
+const randomcolor =  require("randomcolor")
+const pepsi = client.emojis.find("name", "pepsi");
 
 client.on('ready', () => {
     console.log('+help is ready');
@@ -13,45 +15,45 @@ client.on('message', (message) => {
  var messageText = message.content.toUpperCase(); 
     let args = message.content.split(" ").slice(1);
 
-    if(messageText == "+help") {
+    if(messageText == "+HELP") {
         var embed = new Discord.RichEmbed();
             embed.addField("LukeBeforeYouBot", `Help!`, true)
-                .addField("Help for:", `${member.user}`, true)
+                .addField("Help for:", `${message.author.tag}`, true)
                 .setColor(randomcolor())
-                .setFooter(' ', ' ')
+                .setFooter('LukeBeforeYouBot ', 'SWDTeam')
                 .setThumbnail(`https://cdn.discordapp.com/attachments/223033559726686208/469544295326547988/lukebotbutbetter.png`)
                 .setTimestamp()
                 .setURL(``)
-                .addField('Commands', ``, true)
-                .addField('', `Welcome (auto welcomes)`, true)
-                .addField('', `+help (help)`, true)
-                .addField('', `+avatar (get's user's avatar)`, true)
-                .addField('', `+botavatar (get's bot avatar)`, true)
-                .addField('', `+serveravatar (get's server's icon/avatar)`, true)
-                .addField('', `+ished (ished)`, true)
-                .addField('', `+sarah (sarah)`, true)
-                .addField('', `+creators (list of creators/contributors)`, true)
-                .addField('', `+namemc (WIP)`, true)
-                .addField('', `+3dskin (WIP)`, true)
-                .addField('', `+skin (WIP)`, true)
-                .addField('', `+test (a test command for the bot)`, true)
-                .addField('', `+userinfo (info on the user running the command)`, true)
-                .addField('', `-reboot (Creator/Dev only command)`, true)
-                .addField('Text Commands', ``, true)
-                .addField('', `I don't like pepsi`, true)
-                .addField('', `I don't like coke`, true)
-                .addField('', `I don't like water`, true)
-                .addField('', `I don't like milk`, true)
-                .addField('', `Who is Mumbles?`, true)
-                .addField('', `I want water`, true)
-                .addField('', `I like pepsi`, true)
-                .addField('', `What is a Mew?`, true)
-                .addField('', `What is Mew Bot`, true)
-                .addField('', `There are 3 easter egg text commands! See if you can guess any!`, true)
-                .addField('Other/Message Including or Starting With', ``, true)
-                .addField('', `If you start a sentence with "Fortnite" you get a reward!`, true)
-                .addField('', `If you have a message including the word "Yogscast", a special message appears!`, true)
-                .addField('', `If you DM the bot you get a message on top of the command of origin`, true)
+                .addField('***Commands***', ``, true)
+                .addField('Welcome', `(auto welcomes)`, true)
+                .addField('+help', `(This help embed)`, true)
+                .addField('+avatar', `(get's user's avatar)`, true)
+                .addField('+botavatar ', `(get's bot avatar)`, true)
+                .addField('+serveravatar ', `(get's server's icon/avatar)`, true)
+                .addField('+ished', `(ished)`, true)
+                .addField('+sarah ', `(sarah)`, true)
+                .addField('+creators', `(list of creators/contributors)`, true)
+                .addField('+namemc', `(WIP)`, true)
+                .addField('+3dskin', `(WIP)`, true)
+                .addField('+skin', `(WIP)`, true)
+                .addField('+test', `(a test command for the bot)`, true)
+                .addField('+userinfo', `(info on the user running the command)`, true)
+                .addField('-reboot ', `(Creator/Dev only command)`, true)
+                .addField('***Text Commands***', `${pepsi}`, true)
+                .addField('1.', `I don't like pepsi`, true)
+                .addField('2.', `I don't like coke`, true)
+                .addField('3.', `I don't like water`, true)
+                .addField('4.', `I don't like milk`, true)
+                .addField('5.', `Who is Mumbles?`, true)
+                .addField('6.', `I want water`, true)
+                .addField('7.', `I like pepsi`, true)
+                .addField('8.', `What is a Mew?`, true)
+                .addField('9.', `What is Mew Bot`, true)
+                .addField('----', `There are 3 easter egg text commands! See if you can guess any!`, true)
+                .addField('Other/Message Including or Starting With', `/`, true)
+                .addField('1', `If you start a sentence with "Fortnite" you get a reward!`, true)
+                .addField('2', `If you have a message including the word "Yogscast", a special message appears!`, true)
+                .addField('3', `If you DM the bot you get a message on top of the command of origin`, true)
         message.author.send(embed);
     }
 });
