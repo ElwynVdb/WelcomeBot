25e7714e2e7f186c3026d8da076d67f30fd411e5
Josia50 <elwyn.vdb@gmail.com>
2018-07-25 19:15:59 +0200

    reee

--- scripts/modules.js
+++ scripts/modules.js
@@ -13,7 +13,6 @@ const mewanswer = require("./mewanswers.js");
 const help = require("./help.js");
 const answerdm = require("./dmmessage.js");
 const commands = require("./commands.js");
-const music = require("./Music/Musicbot.js");
 
 
 client.login(config.token);
