7435c179533feb2ce1a8a5cbc83483c947f271ef
Mew <thedalekmoduniversemew@gmail.com>
2018-08-23 15:40:52 +0100

    Who's that girl next door living in the haunted mansion?

--- extracmds.js
+++ extracmds.js
@@ -2,7 +2,9 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const express = require('express');
 const config = require("./config.json");
+const fs = require('fs')
 
 client.on('ready', () => {
     console.log('Extra Commands are reggie like my body!');
 })
+
