4d584bdf50d870d572ae0ce2f472c929e0661425
Mew <thedalekmoduniversemew@gmail.com>
2018-08-23 15:37:31 +0100

    *piano*

--- extracmds.js
+++ extracmds.js
@@ -2,3 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const express = require('express');
 const config = require("./config.json");
+
+client.on('ready', () => {
+    console.log('Extra Commands are reggie like my body!');
+})
