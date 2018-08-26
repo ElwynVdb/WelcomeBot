93f5a99d252dad024fc161448dd211c3a25303c6
Josia50 <elwyn.vdb@gmail.com>
2018-08-26 21:58:00 +0200

    fix

--- scripts/commands.js
+++ scripts/commands.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
 client.on("message", async message => {
     if (message.guild === null) return;
     if(message.author.bot) return;
-
+    if(message.content.indexOf(config.prefix) !== 0) return;
 
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
--- scripts/welcome.js
+++ scripts/welcome.js
@@ -5,23 +5,23 @@ const config = require("./config.json")
 const run = require('./cfg.json');
 const fs = require('fs');
 
-var channel = config.welcomechannel
-
 client.on('ready', () => {
  console.log('Welcome/Leave Message enabled!')
 })
 
 client.on("guildMemberAdd", (member) =>  {
+  var channel = config.welcomechannel
   const pepsi = client.emojis.find("name", "pepsi");
   var message = "And don't forget to check <#307553601642037249>!";
   member.guild.channels.find("name", channel).send(`${member.user}, Welcome to hell it's great here pull up a chair and have a pepsi! ${pepsi}\n${message}`); 
-  console.log(`${member.user} Joined`);
+  console.log(`# ${member.user.username} Joined`);
 });
 
 client.on("guildMemberRemove", (member) => {
+    var channel = config.welcomechannel
     var membertag = member.user.tag.slice(0, -5)
     member.guild.channels.find("name", channel).send(`*Takes ${membertag}'s pepsi back*`);
-    console.log(`${member.user} left `);
+    console.log(`# ${member.user.username} left `);
 });
 
   client.login(run.token); 
