d6c1034c920a3be52391e424501ee32f905efb26
Josia50 <elwyn.vdb@gmail.com>
2018-09-23 19:42:43 +0200

    modules

--- scripts/modules.js
+++ scripts/modules.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const run = require('./cfg.json');
 
-const reboot = require("./reboot.js");
 const answers = require("./answers.js");
 const userserverinfo = require("./information.js");
 const easteregg = require("./eastereggs.js");
@@ -15,5 +14,6 @@ const welcome = require("./welcome.js");
 const eval = require("./eval.js");
 const report = require("./report.js");
 //const extracmd = require('./extracmds.js');
+
 
 client.login(run.token);
