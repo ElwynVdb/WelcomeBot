387da511ccb614325c2706629af4ebd2bc99480b
Josia50 <elwyn.vdb@gmail.com>
2018-12-01 10:40:01 +0100

    mhm

--- scripts/staffcommand.js
+++ scripts/staffcommand.js
@@ -21,21 +21,29 @@ client.on("message", (message) => {
         let warn3 = message.guild.roles.find(r => r.name === "Warning 3")
 
         if(message.member.hasPermission('MANAGE_CHANNELS')) {
-           if(!mention1) return message.reply("Please mention a user to warn!")
-           message.delete(120);
+            if(!mention1) {
+                message.reply("Please mention a user to warn!")
+                message.delete(30)
+                return;
+        }
 
            var memid = mention1.user.id
+           var warngiven = message.author.username
 
          if(!warn[memid]) warn[memid] = {
              warns: 0,
              warn1: "No reason",
              warn2: "No reason",
-             warn3: "No reason"
+             warn3: "No reason",
+             warng1: "Unknown",
+             warng2: "Unknown",
+             warng3: "Unknown"
          };
 
          if(warn[memid].warns === 3) {
         message.channel.send(mention1.user.username + " " + "has already 3 warnings!")
         message.delete(30)
+        return;
          }
 
         if(warn[memid].warns !== 3) {
@@ -43,23 +51,26 @@ client.on("message", (message) => {
           fs.writeFileSync("./warnings.json", JSON.stringify(warn, null , 4))
          }
 
-          var reason = command.slice(args[0])
+          var reason = args.join(" ").slice(22);
           if(!reason) {reason = "Not given"}
 
 
 
           if(warn[memid].warns === 1) {
             warn[memid].warn1 = reason
+            warn[memid].warng1 = warngiven
               mention1.addRole(warn1)
               fs.writeFileSync("./warnings.json", JSON.stringify(warn, null , 4))
           }
           if(warn[memid].warns === 2) {
             warn[memid].warn2 = reason
+            warn[memid].warng2 = warngiven
               mention1.addRole(warn2)
               fs.writeFileSync("./warnings.json", JSON.stringify(warn, null , 4))
           }
           if(warn[memid].warns === 3) {
             warn[memid].warn3 = reason
+            warn[memid].warng3 = warngiven
             mention1.addRole(warn3)
             fs.writeFileSync("./warnings.json", JSON.stringify(warn, null , 4))
         }
@@ -71,6 +82,13 @@ client.on("message", (message) => {
         var mention1 = message.mentions.members.first();
         var memid = mention1.user.id
         var em1 = new discord.RichEmbed(); var em2 = new discord.RichEmbed(); var em3 = new discord.RichEmbed();
+
+        if(!mention1) {
+            message.reply("Please mention a user to check warns!")
+            message.delete(30)
+    }
+        
+
 
         em1.setTitle("Warn Info :" + " " + mention1.user.username)
         .addField("Warn Count", warn[memid].warns)
@@ -90,6 +108,24 @@ client.on("message", (message) => {
         .addField("Warning 3", warn[memid].warn3)
         .setFooter("Warn Check")
 
+        var giv1 = new discord.RichEmbed(); var giv2 = new discord.RichEmbed(); var giv3 = new discord.RichEmbed();
+        
+        giv1.setTitle("Warninfo")
+        .addField("Warning 1 by:", warn[memid].warng1)
+        .setFooter("Warninfo")
+
+        giv2.setTitle("Warninfo")
+        .addField("Warning 1 by:", warn[memid].warng1)
+        .addField("Warning 1 by:", warn[memid].warng2)
+        .setFooter("Warninfo")
+
+        giv3.setTitle("Warninfo")
+        .addField("Warning 1 by:", warn[memid].warng1)
+        .addField("Warning 2 by:", warn[memid].warng2)
+        .addField("Warning 3 by:", warn[memid].warng3)
+        .setFooter("Warninfo")
+
+        if(!args[1]) {
         if(warn[memid].warns == 1) {
             message.channel.send(em1)
         }
@@ -100,6 +136,18 @@ client.on("message", (message) => {
             message.channel.send(em3)
         }
     }
+    if(args[1] === "given") {
+        if(warn[memid].warns === 1) {
+        message.channel.send(giv1)
+        }
+        if(warn[memid].warns === 2) {
+            message.channel.send(giv2)
+            }
+            if(warn[memid].warns === 3) {
+                message.channel.send(giv3)
+                }
+    }
+}
 
 })
 
