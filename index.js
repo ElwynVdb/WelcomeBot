f716caad5fdcf80f65424dacf1baa25b04ab9f51
Josia <38124619+Josia50@users.noreply.github.com>
2020-08-09 05:33:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const http = require('http');
 const config = require("./scripts/config.json");
 const run = require('./scripts/cfg.json');
-const express = require('express');
-const app = express();
-var fs = require("fs");
-var request = require('request');
-const randomcolor = require('randomcolor');
-const moment = require('moment');
-
-//To add when using glitch
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
 
 client.on('ready', () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
@@ -27,7 +10,6 @@ client.on('ready', () => {
   const modules = require('./scripts/modules.js');
   // const configedit = require('./configedit.js')
 });
-
 
 client.on("disconnect", () => {
   client.login(run.token);
